d20a239f7883db9e16fcd40e6cf359a6d27def8f
resource100 <resource100>
2017-04-10 01:23:00 +0500

    Fixed router with <Switch> but 404 page not fixed

--- src/components/pageNotFound.tsx
+++ src/components/pageNotFound.tsx
@@ -1,3 +1,3 @@
 export const PageNotFound = () => (
-  <div>Page not found.</div>
+  <div>This is 404 PageNotFound page...</div>
 )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,24 @@
+///<reference path="../node_modules/@types/react-router/index.d.ts"/>
 import * as React from 'react'
-import {BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import {List} from './components/list'
 import {Settings} from './components/settings'
 import {MainApp} from "./components/mainApp";
 import {Login} from "./components/login";
+import {PageNotFound} from './components/pageNotFound'
 
 
 export const Routes = () => (
   <Router>
     <div>
-      <Route path="/login" component={Login}/>
-      <MainApp>
-        <Route path="/list" component={List}/>
-        <Route path="/settings" component={Settings}/>
-      </MainApp>
+      <Switch>
+        <Route exact path="/login" component={Login}/>
+        <MainApp>
+          <Route exact path="/list" component={List}/>
+          <Route exact path="/settings" component={Settings}/>
+        </MainApp>
+        <Route path="*" component={PageNotFound}/>
+      </Switch>
     </div>
   </Router>
 )
