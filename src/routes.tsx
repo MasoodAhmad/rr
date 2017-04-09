6e9b360ee4316ab42fcecc820a29bbfcf34ba897
resource100 <resource100>
2017-04-09 20:52:41 +0500

    Login route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,17 +3,17 @@ import {BrowserRouter as Router, Route } from 'react-router-dom'
 import {List} from './components/list'
 import {Settings} from './components/settings'
 import {MainApp} from "./components/mainApp";
+import {Login} from "./components/login";
 
 
 export const Routes = () => (
   <Router>
     <div>
+      <Route   path="/login" component={Login}/>
       <MainApp>
-        <Route  path="/list" component={List}/>
-        <Route  path="/settings" component={Settings}/>
+        <Route   path="/list" component={List}/>
+        <Route   path="/settings" component={Settings}/>
       </MainApp>
-
-
     </div>
   </Router>
 )
