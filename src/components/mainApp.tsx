04472729159c7490b47d5b65fc44d27ea0a8b769
resource100 <resource100>
2017-04-17 01:21:28 +0500

    Refactored nested routes with RouterContainer

--- src/components/mainApp.tsx
+++ src/components/mainApp.tsx
@@ -1,14 +1,16 @@
 import {Header} from './header'
 import {Footer} from './footer'
+import {Route} from "react-router-dom";
+import * as React from 'react'
 
-export const MainApp = (props) => (
-  <div>This is main app
-    <Header/>
-    {' '}
-    <div>{props.children}</div>
-    <Footer/>
-  </div>
-)
+export const MainApp = ({ component, ...rest }) => (
+    <div>
+      {/*<Header />*/}
+      {/*<Route {...rest} render={ () => React.createElement(component) } />*/}
+      {/*<Footer />*/}
+    </div>
+  )
+
 
 // need header in all the app. Header also has main menu
 // here the list or settings componenet should load
--- src/routes.tsx
+++ src/routes.tsx
@@ -7,27 +7,68 @@ import {MainApp} from "./components/mainApp";
 import {Login} from "./components/login";
 import {PageNotFound} from './components/pageNotFound'
 import {User, UserID, UserStatic} from "./components/User";
+import {Header} from "./components/header";
+import {Footer} from "./components/footer";
 
 
-export const Routes = () => (
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/login" component={Login}/>
-        <MainApp>
-          <Route path="/list" component={List}/>
-          <Route path="/settings" component={Settings}/>
+// export const Routes = () => (
+  {/*<Router>*/}
+    {/*<div>*/}
+      {/*<Switch>*/}
+        {/*<Route exact path="/login" component={Login}/>*/}
+        {/*<MainApp>*/}
+          {/*<Route path="/list" component={List}/>*/}
+          {/*<Route path="/settings" component={Settings}/>*/}
+          {/*<Switch>*/}
+            {/*<User path="/user" >*/}
+              {/*<Switch>*/}
+                {/*<Route exact path="/user/staticUser" component={UserStatic} />*/}
+                {/*<Route exact path="/user/:id" component={UserID} />*/}
+              {/*</Switch>*/}
+            {/*</User>*/}
+          {/*</Switch>*/}
+        {/*</MainApp>*/}
+        {/*<Route path="*" component={PageNotFound}/>*/}
+      {/*</Switch>*/}
+    {/*</div>*/}
+  {/*</Router>*/}
+{/*)*/}
+
+// class App extends Component {
+//   render() {
+//
+//     const Header = () => <h1>My header</h1>;
+//     const Footer = () => <h2>My footer</h2>;
+//     const Login = () => <p>Login Component</p>;
+//     const Home = () => <p>Home Page</p>;
+//     const List = () => <p>List Page</p>;
+//     const Settings = () => <p>Settings Page</p>;
+//     const PageNotFound = () => <h1>Uh oh, not found!</h1>;
+//
+    const MainAppContainer = ({ component, ...rest }) => {
+      return (
+        <div>
+          <Header />
+          <Route {...rest} render={component} />
+          <Footer />
+        </div>
+      );
+    };
+//
+    export const Routes = () => (
+      <Router>
+        <div>
           <Switch>
-            <User path="/user" >
-              <Switch>
-                <Route exact path="/user/staticUser" component={UserStatic} />
-                <Route exact path="/user/:id" component={UserID} />
-              </Switch>
-            </User>
+            <Route exact path="/login" component={Login} />
+            <MainAppContainer exact path="/" component={MainApp} />
+            <MainAppContainer path="/list" component={List} />
+            <MainAppContainer path="/settings" component={Settings} />
+            <Route path="*" component={PageNotFound} />
           </Switch>
-        </MainApp>
-        <Route path="*" component={PageNotFound}/>
-      </Switch>
-    </div>
-  </Router>
-)
+        </div>
+      </Router>
+    );
+//   }
+// }
+//
+// export default App;
